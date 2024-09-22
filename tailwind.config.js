80c39d1b2ba573cb8b60019147b81737f9b27eca
unknown <khadijasheikh266@gmail.com>
2024-09-23 00:43:44 +0600

    navbar and button section done

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        fill: {
+          none: 'none',
+        },
+      },
+    },
+    plugins: [],
+  }
