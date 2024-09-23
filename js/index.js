6fddb00db05b0c0546b9b236612f4e835bec1444
unknown <khadijasheikh266@gmail.com>
2024-09-23 23:37:23 +0600

    till modal done and faq section is running

--- js/index.js
+++ js/index.js
@@ -8,12 +8,19 @@ function getInnerText(id){
     const innText=parseFloat(document.getElementById(id).innerText);
     return innText;
 }
-// hidden function
-function hidden(id){
-    const hide=document.getElementById(id).classList.add('hidden');
-    
-}
+
+// blog button
+const blogBtn=document.getElementById('blog-btn');
+blogBtn.addEventListener('click',function(){
+    window.location.href='./home.html'
+})
+// home button
+const homeBtn=document.getElementById('home-btn');
+homeBtn.addEventListener('click',function(){
+    window.location.href = './index.html';
+})
 // donate now button
+let sum=0;
 const donateNowBtn=document.getElementById('donate-now-btn');
 donateNowBtn.addEventListener('click',function(){
     const mainAmount=getInnerText('total-amount');
@@ -22,15 +29,16 @@ donateNowBtn.addEventListener('click',function(){
     // const donateNoakhali=getInputFiledValue('donate-noakhali');
     if(donateAmount1<=0 || isNaN(donateAmount1)){
         alert('Please Enter The Correct Amount')
+        
     }
     else{
         // donateNoakhali
         const remainingAmount=mainAmount-donateAmount1;
         document.getElementById('total-amount').innerText=remainingAmount;
-        document.getElementById('donate-noakhali').innerText=donateAmount1;
-        // formatCurrency('donate-amount')
-        addToHistory1(donateAmount1);
-        const modal = document.getElementById('my_modal_1');
+        sum=sum+donateAmount1
+        document.getElementById('donate-noakhali').innerText=sum;
+        addToHistory2(donateAmount1);
+        const modal = document.getElementById('my_modal_5');
         modal.showModal();
 
     }
@@ -39,7 +47,7 @@ donateNowBtn.addEventListener('click',function(){
 const donateNowBtn2=document.getElementById('donate-now-btn2');
     donateNowBtn2.addEventListener('click',function(){
     const mainAmount=getInnerText('total-amount')
-    console.log(mainAmount)
+    // console.log(mainAmount)
     const donateAmount2=getInputFiledValue('donate-amount2');
     // const donateNoakhali=getInputFiledValue('donate-noakhali');
     if(donateAmount2<=0 || isNaN(donateAmount2)){
@@ -49,9 +57,10 @@ const donateNowBtn2=document.getElementById('donate-now-btn2');
         // donateNoakhali
         const remainingAmount=mainAmount-donateAmount2;
         document.getElementById('total-amount').innerText=remainingAmount;
-        document.getElementById('donate-feni').innerText=donateAmount2;
+        sum=sum+donateAmount2
+        document.getElementById('donate-feni').innerText=sum;
         addToHistory2(donateAmount2);
-        const modal = document.getElementById('my_modal_1');
+        const modal = document.getElementById('my_modal_5');
         modal.showModal();
 
         // hidden('donate-now-btn2');
@@ -61,13 +70,12 @@ const donateNowBtn2=document.getElementById('donate-now-btn2');
     }
 })
 // // donate now btn3
-let sum=0;
+
 const donateNowBtn3=document.getElementById('donate-now-btn3');
     donateNowBtn3.addEventListener('click',function(){
-    const mainAmount=getInnerText('total-amount')
-    console.log(mainAmount)
+    const mainAmount=getInnerText('total-amount');
     const donateAmount3=getInputFiledValue('donate-amount3');
-    sum=sum+donateAmount3
+    
     // const donateNoakhali=getInputFiledValue('donate-noakhali');
     if(donateAmount3<=0 || isNaN(donateAmount3)){
         alert('Please Enter The Correct Amount')
@@ -76,20 +84,22 @@ const donateNowBtn3=document.getElementById('donate-now-btn3');
         // donateNoakhali
         const remainingAmount=mainAmount-donateAmount3;
         document.getElementById('total-amount').innerText=remainingAmount;
-        document.getElementById('donate-quota').innerText=donateAmount3;
-        addToHistory3(sum);
-        const modal = document.getElementById('my_modal_1');
+        sum=sum+donateAmount3
+        document.getElementById('donate-quota').innerText=sum;
+        addToHistory2(donateAmount3);
+        const modal = document.getElementById('my_modal_5');
         modal.showModal();
         // hidden('donate-now-btn3');
     }
+        
 })
 
 // button colors of history and donate button
 const historyButton=document.getElementById('history-btn');
 const donateButton=document.getElementById('donate-btn');
 historyButton.addEventListener('click',function(){
-    historyButton.classList.add('bg-btn-colors1','text-color')
-    donateButton.classList.remove('bg-btn-colors1','text-color')
+    historyButton.classList.add('bg-btn-colors1')
+    donateButton.classList.remove('bg-btn-colors1')
     // donateButton.classList.add('text-font-color')
     // historyButton.classList.add('bg-btn-colors1,text-color')
     document.getElementById('section-container').classList.add('hidden');
@@ -100,21 +110,14 @@ historyButton.addEventListener('click',function(){
 donateButton.addEventListener('click',function(){
     donateButton.classList.add(
         'bg-btn-colors1',)
-    donateButton.classList.add('bg-btn-colors1','text-color')
-    historyButton.classList.remove('bg-btn-colors1','text-color')
+    donateButton.classList.add('bg-btn-colors1')
+    historyButton.classList.remove('bg-btn-colors1')
     document.getElementById('section-container').classList.remove('hidden');
     document.getElementById('section-container2').classList.remove('hidden');
     document.getElementById('section-container3').classList.remove('hidden');
     document.getElementById('history-section').classList.add('hidden')
 
 })
-// to fixed
-// function formatCurrency(amount){
-//     return `${amount.toFixed(2)}`
-// }
-
-// history
-// section 1
 function addToHistory1(id) {
     const flood1=document.getElementById('noakhali-flood').innerText;
 const historyItem = document.createElement('div');
@@ -131,7 +134,6 @@ historyItem.innerHTML = `
 
 // Get the id from html to add the above elements
 const historyContainer = document.getElementById('history-list');
-console.log(historyContainer);
 historyContainer.insertBefore(historyItem, historyContainer.firstChild);
 }
 // section 2
@@ -150,7 +152,6 @@ historyItem.innerHTML = `
 `;
 // Get the id from html to add the above elements
 const historyContainer = document.getElementById('history-list');
-console.log(historyContainer);
 historyContainer.insertBefore(historyItem, historyContainer.firstChild);
 }
 // section3
@@ -172,7 +173,6 @@ historyContainer.insertBefore(historyItem, historyContainer.firstChild);
 
     // Get the id from html to add the above elements
     const historyContainer = document.getElementById('history-list');
-    console.log(historyContainer);
     historyContainer.appendChild(historyItem);
 }
 // remove hidden and add hidden
